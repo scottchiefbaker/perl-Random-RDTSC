d44e8a8f3ab3e9a75255a9302e4ae43006c4b124
Scott Baker <scott@perturb.org>
2025-02-04 13:51:00 -0800

    Update README

--- README.md
+++ README.md
@@ -15,7 +15,29 @@ print "TSC: $timestamp, Random: $random_val\n";
 
 # DESCRIPTION
 
-This module provides access to the `get_rdtsc()` and `rdtsc_rand64()` functions from the `rdtsc_rand` library, allowing for high-resolution timestamps and random number generation based on the CPU's RDTSC instruction.
+This module provides access to the `get_rdtsc()` and `rdtsc_rand64()`
+functions from the `rdtsc_rand` library, allowing for high-resolution
+timestamps and random number generation based on the CPU's RDTSC instruction.
+
+`Random::RDTSC` is not vetted for true randomness. It is designed to be a
+good starting point for other pseudo random number generators. If you use
+the numbers from `Random::RDTSC` as seeds for other PRNGs you can have good,
+self-starting random numbers.
+
+`Random::RDTSC` is not seedable and thus _not repeatable_. Repeatability
+can be helpful for testing and validating code. You should use a seedable
+PRNG to get repeatable random numbers.
+
+Based on [rdtsc\_rand](https://github.com/scottchiefbaker/rdtsc_rand).
+
+# CAVEATS
+
+## Are there better sources of randomness?
+
+Most definitely. Your operating system gathers a source of entropy for
+randomness that is very high quality. For cryptographic applications and
+anything where true randomness is required you should use those instead.
+`Random::RDTSC` is designed to be quick and simple.
 
 # FUNCTIONS
 
--- lib/Random/RDTSC.pm
+++ lib/Random/RDTSC.pm
@@ -30,7 +30,29 @@ Random::RDTSC - Perl wrapper for RDTSC-based random number generation
 
 =head1 DESCRIPTION
 
-This module provides access to the C<get_rdtsc()> and C<rdtsc_rand64()> functions from the C<rdtsc_rand> library, allowing for high-resolution timestamps and random number generation based on the CPU's RDTSC instruction.
+This module provides access to the C<get_rdtsc()> and C<rdtsc_rand64()>
+functions from the C<rdtsc_rand> library, allowing for high-resolution
+timestamps and random number generation based on the CPU's RDTSC instruction.
+
+C<Random::RDTSC> is not vetted for true randomness. It is designed to be a
+good starting point for other pseudo random number generators. If you use
+the numbers from C<Random::RDTSC> as seeds for other PRNGs you can have good,
+self-starting random numbers.
+
+C<Random::RDTSC> is not seedable and thus I<not repeatable>. Repeatability
+can be helpful for testing and validating code. You should use a seedable
+PRNG to get repeatable random numbers.
+
+Based on L<rdtsc_rand|https://github.com/scottchiefbaker/rdtsc_rand>.
+
+=head1 CAVEATS
+
+=head2 Are there better sources of randomness?
+
+Most definitely. Your operating system gathers a source of entropy for
+randomness that is very high quality. For cryptographic applications and
+anything where true randomness is required you should use those instead.
+C<Random::RDTSC> is designed to be quick and simple.
 
 =head1 FUNCTIONS
 
